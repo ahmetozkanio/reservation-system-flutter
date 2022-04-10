c97adbc14b4861c61e0c4e205b81dcdeb6f6c10c
ahmetozkanio <bledwarrior@gmail.com>
2022-04-10 13:31:23 +0300

    Fakulte List and sehir-ilce list

--- lib/actions/sehirilce/api/ilce_api.dart
+++ lib/actions/sehirilce/api/ilce_api.dart
@@ -1,28 +1,45 @@
+import 'dart:convert';
+
+import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 import 'package:library_reservation_liberta_flutter/actions/sehirilce/models/sehir_model.dart';
 
+import '../../../core/login/auth_manager.dart';
 import '../models/ilce_model.dart';
 import '../models/sehir_model.dart';
 import '/api/base_api.dart';
 
 class IlceApi {
-  static Future<List<Ilce>?> getIlceListApi(int id) async {
+  Future<List<IlceModel>?> getIlceListApi(int sehirId) async {
+    final AuthenticationManager _authenticationManager = Get.find();
+
+    final Uri ilceListUrl = Uri.parse(BaseApi.apiBaseUrl +
+        "SehirIlce/IlceListesi?SehirId=" +
+        sehirId.toString());
     try {
+      var userToken = _authenticationManager.getToken();
+
       var response = await http.post(
-        Uri.parse(
-          BaseApi.apiBaseUrl + "/api/SehirIlce/ilce/list/" + id.toString(),
-        ),
+        ilceListUrl,
+        headers: {
+          'Authorization': 'Bearer $userToken',
+          "contentType": "application/json",
+        },
       );
-      print("ilce : " + response.statusCode.toString());
 
       if (response.statusCode == 200) {
-        var jsonString = response.body;
-        return ilceFromJson(jsonString);
+        Map<String, dynamic> json = jsonDecode(response.body);
+        List<dynamic> body = json['data'];
+
+        List<IlceModel>? data =
+            body.map((dynamic item) => IlceModel.fromJson(item)).toList();
+        return data;
       } else {
         return null;
       }
     } catch (e) {
       print("Ilceler : " + e.toString());
     }
+    return null;
   }
 }
