2ef61c29ee6b04dc4f2c976e465c10d24e91aa01
ahmetozkanio <bledwarrior@gmail.com>
2022-04-18 10:17:22 +0300

    salon card front

--- lib/home/user/home_view_user.dart
+++ lib/home/user/home_view_user.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:library_reservation_liberta_flutter/widgets/background_gradient.dart';
 import 'package:library_reservation_liberta_flutter/widgets/searchList.dart';
 import 'package:flip_card/flip_card.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 
 class HomeViewUser extends StatelessWidget {
   const HomeViewUser({Key? key}) : super(key: key);
@@ -167,7 +168,7 @@ class HomeViewUser extends StatelessWidget {
             ),
             SliverPadding(
               padding: const EdgeInsets.all(
-                16.0,
+                12.0,
               ),
               sliver: SliverGrid(
                 gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
@@ -182,120 +183,8 @@ class HomeViewUser extends StatelessWidget {
                         fill: Fill
                             .fillBack, // Fill the back side of the card to make in the same size as the front.
                         direction: FlipDirection.HORIZONTAL, // default
-                        front: Card(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(11),
-                          ),
-                          child: Container(
-                            decoration: BoxDecoration(
-                              color: Colors.teal[100 * (index % 9)],
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(11),
-                              ),
-                            ),
-                            width: double.infinity,
-                            height: double.infinity,
-                            padding: EdgeInsets.all(12.0),
-                            child: Stack(
-                              children: [
-                                Container(
-                                  alignment: Alignment.topLeft,
-                                  child: Text(
-                                    'SALON ON $index',
-                                  ),
-                                ),
-                                Positioned(
-                                    bottom: 0,
-                                    right: 0,
-                                    child: Container(
-                                      alignment: Alignment.center,
-                                      width: 38,
-                                      height: 38,
-                                      decoration: BoxDecoration(
-                                        color: Color.fromARGB(255, 245, 40, 25),
-                                        borderRadius: BorderRadius.all(
-                                            Radius.elliptical(32, 32)),
-                                      ),
-                                    )),
-                                Positioned(
-                                  bottom: 3,
-                                  right: 3,
-                                  child: Container(
-                                    alignment: Alignment.center,
-                                    width: 32,
-                                    height: 32,
-                                    decoration: BoxDecoration(
-                                      color: Color.fromARGB(255, 255, 255, 255),
-                                      borderRadius: BorderRadius.all(
-                                          Radius.elliptical(32, 32)),
-                                    ),
-                                    child: Text(
-                                      "%98",
-                                      style: TextStyle(fontSize: 11.0),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                        back: Card(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(11),
-                          ),
-                          child: Container(
-                            decoration: BoxDecoration(
-                              color: Colors.teal[100 * (index % 9)],
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(11),
-                              ),
-                            ),
-                            width: double.infinity,
-                            height: double.infinity,
-                            padding: EdgeInsets.all(12.0),
-                            child: Stack(
-                              children: [
-                                Container(
-                                  alignment: Alignment.topLeft,
-                                  child: Text(
-                                    'SALON ARKA $index',
-                                  ),
-                                ),
-                                Positioned(
-                                    bottom: 0,
-                                    right: 0,
-                                    child: Container(
-                                      alignment: Alignment.center,
-                                      width: 38,
-                                      height: 38,
-                                      decoration: BoxDecoration(
-                                        color: Color.fromARGB(255, 245, 40, 25),
-                                        borderRadius: BorderRadius.all(
-                                            Radius.elliptical(32, 32)),
-                                      ),
-                                    )),
-                                Positioned(
-                                  bottom: 3,
-                                  right: 3,
-                                  child: Container(
-                                    alignment: Alignment.center,
-                                    width: 32,
-                                    height: 32,
-                                    decoration: BoxDecoration(
-                                      color: Color.fromARGB(255, 255, 255, 255),
-                                      borderRadius: BorderRadius.all(
-                                          Radius.elliptical(32, 32)),
-                                    ),
-                                    child: Text(
-                                      "%98",
-                                      style: TextStyle(fontSize: 11.0),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ));
+                        front: salonCardFront(index),
+                        back: salonCardBack(index));
                   },
                   childCount: 11,
                 ),
@@ -304,6 +193,240 @@ class HomeViewUser extends StatelessWidget {
           ],
         ),
       );
+
+  Card salonCardBack(
+    int index,
+  ) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(
+          11.0,
+        ),
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.teal[100 * (index % 9)],
+          borderRadius: BorderRadius.all(
+            Radius.circular(
+              11,
+            ),
+          ),
+        ),
+        width: double.infinity,
+        height: double.infinity,
+        padding: EdgeInsets.all(
+          12.0,
+        ),
+        child: Stack(
+          children: [
+            Container(
+              alignment: Alignment.topLeft,
+              child: Text(
+                'SALON ARKA $index',
+              ),
+            ),
+            Positioned(
+                bottom: 0,
+                right: 0,
+                child: Container(
+                  alignment: Alignment.center,
+                  width: 38,
+                  height: 38,
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 245, 40, 25),
+                    borderRadius: BorderRadius.all(Radius.elliptical(32, 32)),
+                  ),
+                )),
+            Positioned(
+              bottom: 3,
+              right: 3,
+              child: Container(
+                alignment: Alignment.center,
+                width: 32,
+                height: 32,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 255, 255, 255),
+                  borderRadius: BorderRadius.all(Radius.elliptical(32, 32)),
+                ),
+                child: Text(
+                  "%98",
+                  style: TextStyle(fontSize: 11.0),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Card salonCardFront(int index) {
+    var dateTimeNow = DateTime.now();
+    return Card(
+      elevation: 3.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(
+          11,
+        ),
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(
+            Radius.circular(
+              11,
+            ),
+          ),
+        ),
+        // width: double.infinity,
+        // height: double.infinity,
+        padding: EdgeInsets.all(
+          12.0,
+        ),
+        child: Stack(
+          children: [
+            Container(
+              alignment: Alignment.topLeft,
+              child: Text(
+                'SALON ON $index',
+              ),
+            ),
+            Positioned(
+              top: 0.0,
+              right: 0.0,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Icon(
+                    Icons.wifi,
+                    color: Colors.grey[400],
+                    size: 16.0,
+                  ),
+                  Icon(
+                    Icons.electrical_services,
+                    color: Colors.grey[400],
+                    size: 16.0,
+                  ),
+                  Icon(
+                    Icons.desktop_mac,
+                    color: Colors.grey[400],
+                    size: 16.0,
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              bottom: 0.0,
+              left: 0.0,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.schedule,
+                        color: Colors.grey[400],
+                        size: 16.0,
+                      ),
+                      SizedBox(
+                        width: 3.0,
+                      ),
+                      Text(
+                        'Rezervasyon:',
+                        style: TextStyle(
+                          color: Colors.grey[600],
+                        ),
+                      ),
+                      SizedBox(
+                        width: 3.0,
+                      ),
+                      Text(
+                        '10:00 - 18/04/2022',
+                        textAlign: TextAlign.right,
+                        style: TextStyle(
+                          color: Colors.grey[600],
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 8.0,
+                  ),
+                  Row(
+                    children: [
+                      // Text(
+                      //   'Kimler Girebilir:',
+                      //   style: TextStyle(
+                      //     color: Colors.grey[600],
+                      //   ),
+                      // ),
+                      //
+                      Icon(
+                        Icons.people_rounded,
+                        color: Colors.grey[400],
+                        size: 16.0,
+                      ),
+                      SizedBox(
+                        width: 3.0,
+                      ),
+                      Text(
+                        'Akademisyen, Oğrenci',
+                        textAlign: TextAlign.right,
+                        style: TextStyle(
+                          color: Colors.grey[600],
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 8.0,
+                  ),
+                  Row(
+                    children: [
+                      // Container(
+                      //   alignment: Alignment.centerRight,
+                      //   child: Text(
+                      //     'Salon Özellikleri:',
+                      //     textAlign: TextAlign.right,
+                      //     style: TextStyle(
+                      //       color: Colors.grey[600],
+                      //     ),
+                      //   ),
+                      // ),
+                      // SizedBox(
+                      //   width: 3.0,
+                      // ),
+                    ],
+                  ),
+                  Row(
+                    children: [],
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              bottom: 0.0,
+              right: 0.0,
+              child: CircularPercentIndicator(
+                radius: 32.0,
+                lineWidth: 7.0,
+                percent: 0.8,
+                animation: true,
+                animationDuration: 2000,
+                center: Text(
+                  "80%",
+                ),
+                progressColor: Colors.green,
+                backgroundColor: Colors.grey[300]!,
+                circularStrokeCap: CircularStrokeCap.round,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
 
 //  Expanded(
